63ffc8c8ce6c3ee3aa8a74b8fb31b41b5d2583d4
kazuya kawaguchi <kawakazu80@gmail.com>
2020-03-09 19:24:50 +0900

    vue-jest version

--- tests/unit/example.spec.js
+++ tests/unit/example.spec.js
@@ -1,12 +1,15 @@
-import { shallowMount } from '@vue/test-utils'
-import HelloWorld from '@/components/HelloWorld.vue'
+import { mount, createLocalVue } from '@vue/test-utils'
+import VueI18n from 'vue-i18n'
+import HelloI18n from '@/components/HelloI18n.vue'
 
-describe('HelloWorld.vue', () => {
-  it('renders props.msg when passed', () => {
-    const msg = 'new message'
-    const wrapper = shallowMount(HelloWorld, {
-      propsData: { msg }
+describe('HelloI18n.vue', () => {
+  it('renders i18n custom blocks', () => {
+    const localVue = createLocalVue()
+    localVue.use(VueI18n)
+    const i18n = new VueI18n({
+      locale: 'en'
     })
-    expect(wrapper.text()).toMatch(msg)
+    const wrapper = mount(HelloI18n, { i18n, localVue })
+    expect(wrapper).toMatchSnapshot()
   })
 })
