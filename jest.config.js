df0360daedb9bf343b8127eab867706525e1268c
kazuya kawaguchi <kawakazu80@gmail.com>
2020-03-09 19:35:24 +0900

    vue-i18n-jest version

--- jest.config.js
+++ jest.config.js
@@ -1,3 +1,7 @@
-module.exports = {
-  preset: '@vue/cli-plugin-unit-jest'
-}
+// NOTE: hack that reads the jest config defined in vue-cli, so that I can reproduce it.
+const config = require('@vue/cli-plugin-unit-jest/jest-preset')
+
+// overrides with 'vue-i18n-jest
+config['transform']['^.+\\.vue$'] = 'vue-i18n-jest'
+
+module.exports = config
